aff71f7003f0a1748fc66d25ceec32375ad34f2c
Maxim Leontyev <leontyevmax@gmail.com>
2012-03-06 15:44:10 +0400

    small corrections

--- upload/library/uLogin/Installer.php
+++ upload/library/uLogin/Installer.php
@@ -1,9 +1,11 @@
 <?php
 class uLogin_Installer{
     protected static $table = array('createQuery' => "CREATE TABLE IF NOT EXISTS ul_user(uid INTEGER, identity VARCHAR(256), 
-                                                      PRIMARY KEY(uid), FOREIGN KEY (uid) REFERENCES xf_user(user_id) ON DELETE CASCADE)
+                                                      PRIMARY KEY(uid))
                                                       ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;",
-                                    'dropQuery' => "DROP TABLE IF EXISTS ul_user;");
+                                    'dropQuery' => "DROP TABLE IF EXISTS ul_user;",
+                                    'deleteQuery' => "DELETE a1 FROM xf_user AS a1 INNER JOIN ul_user AS a2
+                                                      WHERE a1.user_id=a2.uid;");
     
     static function install($existingAddOn,$addOnData){
         $db = XenForo_Application::get('db');
@@ -14,6 +16,7 @@ class uLogin_Installer{
     static function uninstall($data)
     {
         $db = XenForo_Application::get('db');
+        $db->query(self::$table['deleteQuery']);
         $db->query(self::$table['dropQuery']);
         return true;
     }
